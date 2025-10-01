6a19052c65dedb8bee9e64dd501712d5268a9f42
vlad <vkm.blog@yandex.ru>
2025-10-01 21:55:50 +0300

    Добавлена модель конференции

--- students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/conferences/models.py
+++ students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/conferences/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Conference(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    topics = models.ManyToManyField('Topic', blank=True)
+    participation_conditions = models.TextField(blank=True)
+    venue_name = models.CharField(max_length=255)
+    venue_description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['start_date']
+
+    def __str__(self):
+        return f'{self.name} ({self.start_date} — {self.end_date})'
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
