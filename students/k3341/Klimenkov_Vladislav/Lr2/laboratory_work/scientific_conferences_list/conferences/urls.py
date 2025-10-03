c43a649b6614dd1fde0e23bbaa78875a81477477
vlad <vkm.blog@yandex.ru>
2025-10-03 14:45:03 +0300

    Реализована страница с детальной информацией о конференции

--- students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/conferences/urls.py
+++ students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/conferences/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ConferencesListView
+from .views import ConferencesListView, ConferenceDetailView
 
 
 urlpatterns = [
     path('list/', ConferencesListView.as_view(), name='conferences_list'),
+    path('<int:pk>/', ConferenceDetailView.as_view(), name='conference_detail'),
 ]
