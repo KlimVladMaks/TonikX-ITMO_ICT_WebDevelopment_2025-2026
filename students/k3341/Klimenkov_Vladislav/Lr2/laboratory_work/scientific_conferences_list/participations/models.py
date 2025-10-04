cd106f78889a5fdb2ac737eb5f1b9a80dd1994f9
vlad <vkm.blog@yandex.ru>
2025-10-04 20:00:59 +0300

    Создание модели выступления

--- students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/participations/models.py
+++ students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/participations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
+from conferences.models import Conference
 
-# Create your models here.
+
+class Presentation(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.topic} — {self.author}"
