054a9eb0e6c1b93749ee45ddbad34a8740d68203
vlad <vkm.blog@yandex.ru>
2025-10-07 18:38:23 +0300

    Регистрация модели отзывов в админ-панели

--- students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/conferences/admin.py
+++ students/k3341/Klimenkov_Vladislav/Lr2/laboratory_work/scientific_conferences_list/conferences/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Conference, Presentation
+from .models import Conference, Presentation, Review
 
 admin.site.register(Conference)
 admin.site.register(Presentation)
+admin.site.register(Review)
