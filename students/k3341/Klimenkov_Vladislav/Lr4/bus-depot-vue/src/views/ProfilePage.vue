cfbaf3fa408f0b6ec00256c87b45f7df0cbbaed8
vlad <vkm.blog@yandex.ru>
2025-12-24 14:34:15 +0300

    Начало работы над механизмом изменения логина

--- students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/views/ProfilePage.vue
+++ students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/views/ProfilePage.vue
@@ -1,13 +1,25 @@
 <script>
 import Header from '@/components/Header.vue';
+import EditUsername from '@/components/EditUsername.vue';
 
 export default {
     components: {
-        Header
+        Header,
+        EditUsername,
     },
     data() {
         return {
-            username: localStorage.getItem("username")
+            username: localStorage.getItem("username"),
+            showEditUsername: false,
+        }
+    },
+    methods: {
+        handleUsernameUpdated(newUsername) {
+            this.username = newUsername;
+            this.showEditUsername = false;
+        },
+        handleEditCancel() {
+            this.showEditUsername = false;
         }
     },
 }
@@ -17,9 +29,19 @@ export default {
 <template>
     <Header></Header>
     <h1>Профиль пользователя</h1>
-    <p><b>Логин:</b></p>
-    <p>{{ username }}</p>
-    <button>Изменить</button>
-    <p><b>Пароль:</b></p>
-    <button>Изменить</button>
+
+    <EditUsername
+        v-if="showEditUsername"
+        :current-username="username"
+        @username-updated="handleUsernameUpdated"
+        @cancel="handleEditCancel"
+    />
+
+    <div>
+        <p><b>Логин:</b></p>
+        <p>{{ username }}</p>
+        <button>Изменить</button>
+        <p><b>Пароль:</b></p>
+        <button>Изменить</button>
+    </div>
 </template>
