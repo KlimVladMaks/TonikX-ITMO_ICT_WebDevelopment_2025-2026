0bf21c445a21672070a46c3eb62835d284c29427
vlad <vkm.blog@yandex.ru>
2025-12-24 23:45:21 +0300

    Теперь username изменить нельзя

--- students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/views/ProfilePage.vue
+++ students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/views/ProfilePage.vue
@@ -1,28 +1,15 @@
 <script>
 import Header from '@/components/Header.vue';
-import EditUsername from '@/components/EditUsername.vue';
 
 export default {
     components: {
         Header,
-        EditUsername,
     },
     data() {
         return {
-            username: localStorage.getItem("username"),
-            showEditUsername: false,
+            username: localStorage.getItem("username")
         }
-    },
-    methods: {
-        handleUsernameUpdated(newUsername) {
-            this.username = newUsername;
-            this.showEditUsername = false;
-            this.$refs.header.updateUsername();
-        },
-        handleEditCancel() {
-            this.showEditUsername = false;
-        }
-    },
+    }
 }
 </script>
 
@@ -30,20 +17,8 @@ export default {
 <template>
     <Header ref="header"></Header>
     <h1>Профиль пользователя</h1>
-
-    <EditUsername
-        v-if="showEditUsername"
-        :current-username="username"
-        @username-updated="handleUsernameUpdated"
-        @cancel="handleEditCancel"
-    />
-
-    <div v-else>
-        <p><b>Логин:</b></p>
-        <p>{{ username }}</p>
-        <button @click="showEditUsername = true">Изменить</button>
-
-        <p><b>Пароль:</b></p>
-        <button>Изменить</button>
-    </div>
+    <p><b>Логин:</b></p>
+    <p>{{ username }}</p>
+    <p><b>Пароль:</b></p>
+    <button>Изменить</button>
 </template>
