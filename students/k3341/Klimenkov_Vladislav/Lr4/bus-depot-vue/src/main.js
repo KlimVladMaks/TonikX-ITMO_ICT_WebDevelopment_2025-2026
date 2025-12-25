287dee5e1a3fc6c47d69fac5320bc63afc511d65
vlad <vkm.blog@yandex.ru>
2025-12-25 16:55:42 +0300

    Подключение Vuetify

--- students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/main.js
+++ students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/main.js
@@ -3,4 +3,17 @@ import App from './App.vue';
 import router from "./router";
 import './assets/main.css';
 
-createApp(App).use(router).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App);
+app.use(router);
+app.use(vuetify)
+app.mount('#app');
--- students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/views/AuthPage.vue
+++ students/k3341/Klimenkov_Vladislav/Lr4/bus-depot-vue/src/views/AuthPage.vue
@@ -1,8 +1,25 @@
 <template>
-    <div>
-        <h1>Автобусный парк</h1>
-        <a href="/login">Войти</a>
-        <br>
-        <a href="/signup">Регистрация</a>
-    </div>
+    <v-container class="d-flex flex-column align-center justify-center">
+        <h1 class="mb-6">Автобусный парк</h1>
+
+        <div class="d-flex flex-column" style="width: 300px;">
+            <v-btn
+                to="/login"
+                block
+                color="primary"
+            >
+                Войти
+            </v-btn>
+
+            <br>
+
+            <v-btn
+                to="/signup"
+                block
+                color="primary"
+            >
+                Регистрация
+            </v-btn>
+        </div>
+    </v-container>
 </template>
